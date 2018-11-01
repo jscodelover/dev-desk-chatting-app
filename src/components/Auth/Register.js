9381fb0e8470dde44be666bdbc570d256a8da99d
manisha1896 <manisha.4918@gmail.com>
2018-11-01 11:12:06 +0530

    firebase signup and form verification

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -19,7 +19,9 @@ class Registers extends Component {
       username: "",
       email: "",
       password: "",
-      passwordConfirmation: ""
+      passwordConfirmation: "",
+      loading: false,
+      error: []
     };
   }
 
@@ -27,16 +29,70 @@ class Registers extends Component {
     this.setState({ [event.target.name]: event.target.value });
   };
 
+  displayError = error => error.map((err, i) => <p key={i}>{err}</p>);
+
+  errorHighlighter = (error, name) =>
+    error.some(err => err.toLowerCase().includes(name));
+
   handleSubmit = event => {
     event.preventDefault();
+    if (this.isFormValid(this.state)) {
+      this.setState({ loading: true, error: [] });
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then(data => {
+          console.log(data);
+          this.setState({ loading: false });
+        })
+        .catch(error => {
+          console.log(error);
+          this.setState({ error: [error.message], loading: false });
+        });
+    }
+  };
+
+  isFormValid = ({
+    username,
+    email,
+    password,
+    passwordConfirmation,
+    error
+  }) => {
+    let err = [];
+    if (
+      !username.length ||
+      !email.length ||
+      !password.length ||
+      !passwordConfirmation.length
+    ) {
+      this.setState({ error: err.concat("Form is not fill") });
+      return false;
+    } else if (password.length < 6 || passwordConfirmation.length < 6) {
+      this.setState({
+        error: err.concat("Password Should be more than 6 digits")
+      });
+      return false;
+    } else if (password !== passwordConfirmation) {
+      this.setState({ error: err.concat("Password does not match") });
+      return false;
+    }
+    return true;
   };
 
   render() {
-    const { username, email, password, passwordConfirmation } = this.state;
+    const {
+      username,
+      email,
+      password,
+      passwordConfirmation,
+      loading,
+      error
+    } = this.state;
     return (
       <Grid verticalAlign="middle" textAlign="center" className="heading">
         <Grid.Column className="register-box">
-          <Header as="h2" icon color="red" textAlign="center">
+          <Header as="h1" icon color="red" textAlign="center">
             <Icon name="code" />
             Register for Dev Desk
             <Header.Subheader>
@@ -62,15 +118,19 @@ class Registers extends Component {
                 placeholder="Email"
                 onChange={this.handleChange}
                 value={email}
+                className={this.errorHighlighter(error, "email") ? "error" : ""}
               />
               <Form.Input
                 name="password"
                 type="password"
                 icon="lock"
                 iconPosition="left"
-                placeholder="Password"
+                placeholder="Password (with min 6 digit)"
                 onChange={this.handleChange}
                 value={password}
+                className={
+                  this.errorHighlighter(error, "password") ? "error" : ""
+                }
               />
               <Form.Input
                 name="passwordConfirmation"
@@ -80,11 +140,26 @@ class Registers extends Component {
                 placeholder="Confirm Password"
                 onChange={this.handleChange}
                 value={passwordConfirmation}
+                className={
+                  this.errorHighlighter(error, "password") ? "error" : ""
+                }
               />
-              <Button fluid type="submit" color="red">
+              <Button
+                fluid
+                type="submit"
+                color="red"
+                disabled={loading}
+                className={loading ? "loading" : ""}
+              >
                 Submit
               </Button>
             </Segment>
+            {error.length > 0 && (
+              <Message negative>
+                <Message.Header>Error</Message.Header>
+                {this.displayError(error)}
+              </Message>
+            )}
           </Form>
           <Message>
             Already a user ? <Link to="/login">Login</Link>
