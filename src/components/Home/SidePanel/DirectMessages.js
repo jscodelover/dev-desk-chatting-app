2b2c74aa8857204e0f3616805e13d0b1365e1147
manisha1896 <manisha.4918@gmail.com>
2018-12-05 23:15:44 +0530

    lastseen online/offline functionality

--- src/components/Home/SidePanel/DirectMessages.js
+++ src/components/Home/SidePanel/DirectMessages.js
@@ -9,40 +9,64 @@ class DirectMessage extends React.Component{
             userRef: firebase.database().ref('users'),
             connectionRef: firebase.database().ref('.info/connected'),
             persence: firebase.database().ref('presence'),
-            totalUser: []
+            totalUser: [],
+            totalUserStatus: []
         }
     }
 
     componentDidMount(){
         const { userRef, connectionRef, persence } = this.state;
+        const { user } = this.props;
         let loadedUsers = [];
-        userRef.on("child_added", snap => {
-            if(this.props.user.userID !== snap.val().userID)
-            {
-                loadedUsers.push(snap.val())
-                this.setState({totalUser: loadedUsers});
-            }     
-        })
+        userRef.on("value", snap => {
+            for(let key in snap.val()){
+                if(user.userID !== snap.val()[key].userID){
+                    loadedUsers.push(snap.val()[key])
+                }
+            }
+            this.setState({totalUser: loadedUsers});
+        });
 
         connectionRef.on('value', snap => {
-            persence.child()
-        })
+           let status = persence.push({ [user.userID] : snap.val()});
+           status.onDisconnect().remove();
+           status.set({ [user.userID] : true});
+           firebase.database().ref(`users/${user.userID}`).onDisconnect().set({ ...user, lastSeen : firebase.database.ServerValue.TIMESTAMP});
+        });
 
-        
+        persence.on("value", snap => {
+            let persence= [];
+            for(let key in snap.val()){
+                persence.push(snap.val()[key])
+            }
+            this.setState({totalUserStatus: persence});
+        });
+    }
+
+    isOnline = (userID) => {
+        this.state.totalUserStatus.forEach(status => {
+            return status.hasOwnProperty(userID);
+        });
     }
 
     displayUsers = totalUser => 
         totalUser.length && (
             totalUser.map(user => {
                 return (
-                    <Menu.Item>
-                        <span></span> {` `} {user.username}
+                    <Menu.Item
+                        key={user.userID}
+                        active={true}
+                        onClick={() => {console.log(user.username)}}
+                    >
+                        <span> {` `} {user.username} </span>
+                        <Icon name="circle" color={this.isOnline(user.userID) ? "green" : "red"} />
                     </Menu.Item>
                 )
             })
         )
 
     render(){
+        console.log(this.state);
         const { totalUser } = this.state;
         return(
             <Menu.Menu style={{marginTop: '2rem'}}>
