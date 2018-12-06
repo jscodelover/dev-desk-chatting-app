57fa51600ebee6bc71617a714a48863f977e8159
manisha1896 <manisha.4918@gmail.com>
2018-12-06 10:55:29 +0530

    minor update

--- src/components/Home/Messages/MessageHeader.js
+++ src/components/Home/Messages/MessageHeader.js
@@ -11,7 +11,12 @@ class MessageHeader extends React.Component {
       <Segment clearing className="messageHeader">
         <Header as="h2" floated="left" fluid="true" style={{ marginBottom: 0 }}>
           <span>
-            {channelName} <Icon name={"star outline"} color="black" />
+            {channelName}
+            {this.privateChannel ? (
+              ""
+            ) : (
+              <Icon name={"star outline"} color="black" />
+            )}
           </span>
           <Header.Subheader>
             {usersInChannel.length > 1
--- src/components/Home/SidePanel/DirectMessages.js
+++ src/components/Home/SidePanel/DirectMessages.js
@@ -1,82 +1,105 @@
-import React from 'react';
-import { Menu, Icon } from 'semantic-ui-react';
-import firebase from '../../../firebaseConfig';
+import React from "react";
+import { Menu, Icon } from "semantic-ui-react";
+import { connect } from "react-redux";
+import firebase from "../../../firebaseConfig";
+import { setChannel } from "../../../store/action";
 
-class DirectMessage extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={
-            userRef: firebase.database().ref('users'),
-            connectionRef: firebase.database().ref('.info/connected'),
-            persence: firebase.database().ref('presence'),
-            totalUser: [],
-            totalUserStatus: []
+class DirectMessage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userRef: firebase.database().ref("users"),
+      connectionRef: firebase.database().ref(".info/connected"),
+      persence: firebase.database().ref("presence"),
+      totalUser: [],
+      totalUserStatus: [],
+      activeChannel: ""
+    };
+  }
+
+  componentDidMount() {
+    const { userRef, connectionRef, persence } = this.state;
+    const { user } = this.props;
+    let loadedUsers = [];
+    userRef.on("value", snap => {
+      for (let key in snap.val()) {
+        if (user.userID !== snap.val()[key].userID) {
+          loadedUsers.push(snap.val()[key]);
         }
-    }
+      }
+      this.setState({ totalUser: loadedUsers });
+    });
 
-    componentDidMount(){
-        const { userRef, connectionRef, persence } = this.state;
-        const { user } = this.props;
-        let loadedUsers = [];
-        userRef.on("value", snap => {
-            for(let key in snap.val()){
-                if(user.userID !== snap.val()[key].userID){
-                    loadedUsers.push(snap.val()[key])
-                }
-            }
-            this.setState({totalUser: loadedUsers});
-        });
+    connectionRef.on("value", snap => {
+      let status = persence.push({ [user.userID]: snap.val() });
+      status.onDisconnect().remove();
+      status.set({ [user.userID]: true });
+      firebase
+        .database()
+        .ref(`users/${user.userID}`)
+        .onDisconnect()
+        .set({ ...user, lastSeen: firebase.database.ServerValue.TIMESTAMP });
+    });
 
-        connectionRef.on('value', snap => {
-           let status = persence.push({ [user.userID] : snap.val()});
-           status.onDisconnect().remove();
-           status.set({ [user.userID] : true});
-           firebase.database().ref(`users/${user.userID}`).onDisconnect().set({ ...user, lastSeen : firebase.database.ServerValue.TIMESTAMP});
-        });
+    persence.on("value", snap => {
+      let persence = [];
+      for (let key in snap.val()) {
+        persence.push(snap.val()[key]);
+      }
+      this.setState({ totalUserStatus: persence });
+    });
+  }
 
-        persence.on("value", snap => {
-            let persence= [];
-            for(let key in snap.val()){
-                persence.push(snap.val()[key])
-            }
-            this.setState({totalUserStatus: persence});
-        });
-    }
+  isOnline = userID => {
+    this.state.totalUserStatus.forEach(status => {
+      return status.hasOwnProperty(userID);
+    });
+  };
 
-    isOnline = (userID) => {
-        this.state.totalUserStatus.forEach(status => {
-            return status.hasOwnProperty(userID);
-        });
-    }
+  //TODO: active channel need to make global so that their only 1 highlighted channel.
+  changeChannel = user => {
+    this.setState({ activeChannel: user.userID });
+    this.props.setChannel({ channelName: user.username, id: user.userID });
+  };
 
-    displayUsers = totalUser => 
-        totalUser.length && (
-            totalUser.map(user => {
-                return (
-                    <Menu.Item
-                        key={user.userID}
-                        active={true}
-                        onClick={() => {console.log(user.username)}}
-                    >
-                        <span> {` `} {user.username} </span>
-                        <Icon name="circle" color={this.isOnline(user.userID) ? "green" : "red"} />
-                    </Menu.Item>
-                )
-            })
-        )
+  displayUsers = totalUser =>
+    totalUser.length &&
+    totalUser.map(user => {
+      return (
+        <Menu.Item
+          key={user.userID}
+          active={user.userID === this.state.activeChannel}
+          onClick={() => {
+            this.changeChannel(user);
+          }}
+        >
+          <span>{user.username}</span>
+          <Icon
+            name="circle"
+            color={this.isOnline(user.userID) ? "green" : "red"}
+          />
+        </Menu.Item>
+      );
+    });
 
-    render(){
-        console.log(this.state);
-        const { totalUser } = this.state;
-        return(
-            <Menu.Menu style={{marginTop: '2rem'}}>
-                <Menu.Item>
-                    <span><Icon name="envelope" /></span>{` `} Direct Messages {` `} ({totalUser.length})
-                </Menu.Item>
-                {this.displayUsers(totalUser)}
-            </Menu.Menu>
-        );
-    }
+  render() {
+    console.log(this.state);
+    const { totalUser } = this.state;
+    return (
+      <Menu.Menu style={{ marginTop: "2rem" }}>
+        <Menu.Item>
+          <span>
+            <Icon name="envelope" />
+          </span>
+          {` `} Direct Messages {` `} ({totalUser.length})
+        </Menu.Item>
+        {this.displayUsers(totalUser)}
+      </Menu.Menu>
+    );
+  }
 }
 
-export default DirectMessage;
+export default connect(
+  null,
+  { setChannel }
+)(DirectMessage);
