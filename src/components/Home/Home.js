a628b3ea2f5e343ce889e784404cbe4c7e539a27
manisha1896 <manisha.4918@gmail.com>
2019-01-15 19:55:22 +0530

    add permission for notification

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,30 +1,56 @@
 import React from "react";
 import { Grid } from "semantic-ui-react";
 import { connect } from "react-redux";
+import firebase from "../../firebaseConfig";
 import ColorPanel from "./ColorPanel/ColorPanel";
 import SidePanel from "./SidePanel/SidePanel";
 import Messages from "./Messages/Messages";
 import MetaPanel from "./MetaPanel/MetaPanel";
 
-const Home = ({ user, channel }) => {
-  return (
-    <Grid
-      columns="equal"
-      style={{ background: "#eee", marginTop: "0px", height: "100vh" }}
-    >
-      <ColorPanel />
-      {user.userID && <SidePanel />}
+class Home extends React.Component {
+  componentDidMount() {
+    firebase
+      .messaging()
+      .requestPermission()
+      .then(() =>
+        firebase
+          .messaging()
+          .getToken()
+          .then(token => {
+            firebase
+              .database()
+              .ref("/tokens")
+              .push({
+                token: token,
+                uid: firebase.auth().currentUser.uid
+              });
+          })
+      )
+      .catch(err => {
+        console.log("error getting permission :(");
+      });
+  }
+  render() {
+    const { user, channel } = this.props;
+    return (
+      <Grid
+        columns="equal"
+        style={{ background: "#eee", marginTop: "0px", height: "100vh" }}
+      >
+        <ColorPanel />
+        {user.userID && <SidePanel />}
 
-      <Grid.Column style={{ marginLeft: "315px", position: "relative" }}>
-        {channel.id && <Messages/>}
-      </Grid.Column>
+        <Grid.Column style={{ marginLeft: "315px", position: "relative" }}>
+          {channel.id && <Messages />}
+        </Grid.Column>
 
-      <Grid.Column width={4}>
-        <MetaPanel />
-      </Grid.Column>
-    </Grid>
-  );
-};
+        <Grid.Column width={4}>
+          <MetaPanel />
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
 
 const mapStateToProps = ({ user, channel }) => {
   return {
