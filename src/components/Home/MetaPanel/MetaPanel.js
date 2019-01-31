5b2d7aac0df878863bc2a9c2d08e3d6499300f46
manisha1896 <manisha.4918@gmail.com>
2019-01-31 23:03:34 +0530

    feat(metapanel): setup

--- src/components/Home/MetaPanel/MetaPanel.js
+++ src/components/Home/MetaPanel/MetaPanel.js
@@ -1,5 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
+import { Accordion, Icon } from "semantic-ui-react";
 
-export default function MetaPanel() {
-  return <div>Meta Panel</div>;
+export default class MetaPanel extends Component {
+  state = { activeIndex: 0 };
+
+  handleClick = (e, titleProps) => {
+    const { index } = titleProps;
+    const { activeIndex } = this.state;
+    const newIndex = activeIndex === index ? -1 : index;
+
+    this.setState({ activeIndex: newIndex });
+  };
+
+  render() {
+    const { activeIndex } = this.state;
+
+    return (
+      <Accordion styled>
+        <Accordion.Title
+          active={activeIndex === 0}
+          index={0}
+          onClick={this.handleClick}
+        >
+          <Icon name="dropdown" />
+          About Channel
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 0}>
+          <p>sample</p>
+        </Accordion.Content>
+
+        <Accordion.Title
+          active={activeIndex === 1}
+          index={1}
+          onClick={this.handleClick}
+        >
+          <Icon name="dropdown" />
+          All Users
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 1}>
+          <p>sample</p>
+        </Accordion.Content>
+
+        <Accordion.Title
+          active={activeIndex === 2}
+          index={2}
+          onClick={this.handleClick}
+        >
+          <Icon name="dropdown" />
+          Creator
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 2}>
+          <p>sample</p>
+        </Accordion.Content>
+      </Accordion>
+    );
+  }
 }
