3b1f0b4f738a91a7a6f7d062df39bd48e4d8efdf
manisha1896 <manisha.4918@gmail.com>
2019-02-01 21:23:19 +0530

    style(directmessages): console.log remove

--- src/util/directmessage.js
+++ src/util/directmessage.js
@@ -1,7 +1,4 @@
 export default function generateId(user, props) {
-  let value =
-    user.userID > props ? `${user.userID}${props}` : `${props}${user.userID}`;
-  console.log(value);
   return user.userID > props
     ? `${user.userID}${props}`
     : `${props}${user.userID}`;
