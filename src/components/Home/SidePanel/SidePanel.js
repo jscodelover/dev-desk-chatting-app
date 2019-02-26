72a20426da94102e61a2f9e0710ffbf12b7e68fd
manisha1896 <manisha.4918@gmail.com>
2019-02-26 17:12:47 +0530

    refactor(sidepannel): make chnages for mobile display

--- src/components/Home/SidePanel/SidePanel.js
+++ src/components/Home/SidePanel/SidePanel.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Menu } from "semantic-ui-react";
+import { Menu, Sidebar } from "semantic-ui-react";
 import UserPanel from "./UserPanel";
 import Channel from "./Channel";
 import DirectMessage from "./DirectMessages";
@@ -15,12 +15,27 @@ class SidePanel extends Component {
       fontSize: "1.3rem"
     };
     return (
-      <Menu inverted fixed="right" vertical style={{ ...style }}>
+      <Sidebar
+        as={Menu}
+        direction="right"
+        animation="overlay"
+        icon="labeled"
+        inverted
+        vertical
+        visible
+        style={{ ...style }}
+      >
         <UserPanel user={user} />
         {user["starred"] ? <Starred /> : null}
         <Channel user={user} />
         <DirectMessage user={user} />
-      </Menu>
+      </Sidebar>
+      // <Menu inverted fixed="right" vertical style={{ ...style }}>
+      //   <UserPanel user={user} />
+      //   {user["starred"] ? <Starred /> : null}
+      //   <Channel user={user} />
+      //   <DirectMessage user={user} />
+      // </Menu>
     );
   }
 }
