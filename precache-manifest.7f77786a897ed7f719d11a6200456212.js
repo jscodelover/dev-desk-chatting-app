6354507913ccdc7323aba1422a45dee5bd93f52e
manisha1896 <manisha.4918@gmail.com>
2019-04-14 22:21:42 +0530

    Updates

---
+++ precache-manifest.7f77786a897ed7f719d11a6200456212.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/dev-desk-chatting-app/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "77c942c47bb7edf7404e",
+    "url": "/dev-desk-chatting-app/static/css/main.c4a5fdf7.chunk.css"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/dev-desk-chatting-app/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "7da4cd5e19b9f3137715",
+    "url": "/dev-desk-chatting-app/static/js/1.7da4cd5e.chunk.js"
+  },
+  {
+    "revision": "f0e84f8b49cb89eade16",
+    "url": "/dev-desk-chatting-app/static/js/runtime~main.f0e84f8b.js"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/dev-desk-chatting-app/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/dev-desk-chatting-app/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/dev-desk-chatting-app/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/dev-desk-chatting-app/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/dev-desk-chatting-app/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/dev-desk-chatting-app/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "77c942c47bb7edf7404e",
+    "url": "/dev-desk-chatting-app/static/js/main.77c942c4.chunk.js"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/dev-desk-chatting-app/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/dev-desk-chatting-app/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/dev-desk-chatting-app/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/dev-desk-chatting-app/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/dev-desk-chatting-app/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/dev-desk-chatting-app/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/dev-desk-chatting-app/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/dev-desk-chatting-app/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "7da4cd5e19b9f3137715",
+    "url": "/dev-desk-chatting-app/static/css/1.702d014d.chunk.css"
+  },
+  {
+    "revision": "63454c9075cc2de64f027b6ca96b9a11",
+    "url": "/dev-desk-chatting-app/index.html"
+  }
+];
